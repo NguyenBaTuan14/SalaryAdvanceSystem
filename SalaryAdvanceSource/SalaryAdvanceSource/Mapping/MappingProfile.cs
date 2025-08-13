2ed437e0e43a743d05b321ff2cea408301024358
Thuỳ Dương <duongpt261@gmail.com>
2025-08-13 15:11:44 +0700

    duongpt fix type active

--- SalaryAdvanceSource/SalaryAdvanceSource/Mapping/MappingProfile.cs
+++ SalaryAdvanceSource/SalaryAdvanceSource/Mapping/MappingProfile.cs
@@ -8,7 +8,12 @@ namespace SalaryAdvanceSource.Mapping
     {
         public MappingProfile() 
         {
-            CreateMap<Users, GetUserDto>();
+            CreateMap<Users, GetUserDto>()
+                .ForMember(dest => dest.IsActive,
+                           opt => opt.MapFrom(src => Enum.Parse<ActiveStatus>(src.IsActive.ToString())))
+                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Gender));
+
             CreateMap<Departments, GetDepartmentDto>();
             CreateMap<Users, CreateUserDto>();
             CreateMap<Information, GetInfo>();
